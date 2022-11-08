1d68e99734b6d6c4b2b09b159b4f6a69e695d0ef
paulmcgonigle67 <s00210326@atu.ie>
2022-11-08 12:40:43 +0000

    Initial 2

--- app/src/main/java/com/example/stepcountertimer/MainActivity.java
+++ app/src/main/java/com/example/stepcountertimer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.stepcountertimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -12,7 +13,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener  {
     // experimental values for hi and lo magnitude limits
     private final double HI_STEP = 11.0;     // upper mag limit
     private final double LO_STEP = 8.0;      // lower mag limit
@@ -20,16 +22,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     int counter1 = 0;                // step counter
 
     CountUpTimer timer;
-    TextView counter;
+    TextView timecounter;
 
     Button startBtn ;
     Button pauseBtn;
-//    Button resumeBtn1;
 
-    //STEPS STUFF
-    TextView tvSteps;
+    //steps stuff
+    TextView tvStepsNum;
+
     private SensorManager mSensorManager;
     private Sensor mSensor;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,23 +48,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 //       resumeBtn1.setVisibility(View.GONE);
 
         //TIMER STUFF
-        counter = findViewById(R.id.tvCount);
-        timer = new CountUpTimer(300000) {  // should be high for the run (ms)
+        timecounter = findViewById(R.id.tvCount);
+        timer = new CountUpTimer(1000000) {  // should be high for the run (ms)
             public void onTick(int second) {
-                counter.setText(String.valueOf(second));
+                timecounter.setText(String.valueOf(second));
             }
         };
 
-        //STEPS STUFF
 
-        tvSteps = findViewById(R.id.tvstepsNum);
-
+        //COUNTER STUFF
+        tvStepsNum = findViewById(R.id.tvstepsNum);
         // we are going to use the sensor service
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
     }
 
+//BUTTON FOR SSTART
     public void doStart(View view) {
         timer.start();
         Toast.makeText(this, "Started counting", Toast.LENGTH_SHORT).show();
@@ -69,7 +72,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         startBtn.setVisibility((View.GONE));
 
     }
-
+//BUTTON FOR STOP
     public void doStop(View view) {
         timer.cancel();
         Toast.makeText(this, "Stopped Run", Toast.LENGTH_SHORT).show();
@@ -79,6 +82,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
     }
+    //PAUSE BUTTON NOT IMPLEMETNED
     public void doResume(View view){
         timer.start();
 //        resumeBtn1.setVisibility(View.GONE);
@@ -86,25 +90,50 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
+    //RESET BUTTON
     public void doReset(View view) {
-        counter.setText("0");
+        timer.cancel();
+        timecounter.setText("0");
+
         Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show();
         startBtn.setVisibility(View.VISIBLE);
     }
 
-    /*
-     * When the app is brought to the foreground - using app on screen
-     */
+    public void ShowStats(View view){
+        Intent intent = new Intent(this, StatsPage.class);
+        startActivity(intent);
+        String steps = tvStepsNum.toString();
+
+        intent.putExtra("stepstaken", String.valueOf(tvStepsNum) );
+        intent.putExtra("timetaken", String.valueOf(timecounter));
+        startActivity(intent);
+    }
+//TIMER STUFF
+    protected void onResume() {
+        super.onResume();
+        // turn on the sensor
+        mSensorManager.registerListener(this, mSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);    // turn off listener to save power
+    }
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+
         float x = sensorEvent.values[0];
         float y = sensorEvent.values[1];
         float z = sensorEvent.values[2];
+
+
 
         // get a magnitude number using Pythagorus's Theorem
         double mag = round(Math.sqrt((x*x) + (y*y) + (z*z)), 2);
 
 
+        // for me! if msg > 11 and then drops below 9, we have a step
         // you need to do your own mag calculating
         if ((mag > HI_STEP) && (highLimit == false)) {
             highLimit = true;
@@ -112,14 +141,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if ((mag < LO_STEP) && (highLimit == true)) {
             // we have a step
             counter1++;
-            tvSteps.setText(String.valueOf(counter1));
+            tvStepsNum.setText(String.valueOf(counter1));
             highLimit = false;
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
-//not used
+
     }
     public static double round(double value, int places) {
         if (places < 0) throw new IllegalArgumentException();
